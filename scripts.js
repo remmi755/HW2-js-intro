bfc2a542354991b341efd6bd83aa541772108586
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-10-28 14:49:59 +0300

    first part of that task

---
+++ scripts.js
@@ -0,0 +1,73 @@
+console.log('------------------#5');
+let a = 4;
+let b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log('------------------#6');
+for (let i = 1; i < 6; i++) {
+console.log(i);
+}
+console.log('------------------#7');
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log('-------------------#8');
+function getMarkInfo(condition) {
+
+   if (condition !== 10) {
+       console.log('У вас средний балл');
+   } else {
+       console.log('У вас максимальный балл');
+
+   }
+}
+getMarkInfo(10)
+getMarkInfo(1)
+console.log('---------------------#9');
+
+function getDayInfo(position) {
+    switch (position) {
+        case 0 :
+            console.log('Sunday');
+            break;
+        case 1 :
+            console.log('Monday');
+            break;
+        case 2 :
+            console.log('Tuesday');
+            break;
+        case 3 :
+            console.log('Wednesday');
+            break;
+        case 4 :
+            console.log('Thursday');
+            break;
+        case 5 :
+            console.log('Friday');
+            break;
+        case 6 :
+            console.log('Saturday');
+            break;
+        default:
+            console.log('Wrong data');
+            break;
+    }
+}
+getDayInfo(0)
+getDayInfo(1)
+getDayInfo(2)
+getDayInfo(3)
+getDayInfo(4)
+getDayInfo(5)
+getDayInfo(6)
+console.log('---------------------#10');
+let arr = ['a','b','c']
+arr.push(1,2,3);
+console.log(arr);
+console.log('---------------------#11');
+let arrOne = [1,2,3];
+let arrTwo = [4,5,6];
+let arrNew = arrOne.concat(arrTwo);
+console.log(arrNew);
